fa00f3e52f41e6951a21d384737a21fbc953945a
Adam Krol <adam.dawid.krol@gmail.com>
2024-01-15 14:51:14 +0100

    feature/dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +9,15 @@ export default defineConfig({
   //     src: "/src/",
   //   },
   // },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-   },
-})
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+});
