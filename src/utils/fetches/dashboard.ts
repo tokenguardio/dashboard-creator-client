3a9ef7234a3253bbc83837444d56014c1febb224
rrozek <jakubrojek@gmail.com>
2024-03-21 09:30:45 +0100

    fix env

--- src/utils/fetches/dashboard.ts
+++ src/utils/fetches/dashboard.ts
@@ -17,7 +17,7 @@ export const fetchElementDataCustomQuery = async (dashboardId, formattedElementI
 }
 
 export const fetchElementDataBasicQuery = async (database, schema, table, bodyRequest) => {
-  const response = await axios.post(`${process.env.API_BASE_URL}/api/database-info/generate-chart-data/${database}/${schema}/${table}`, bodyRequest)
+  const response = await axios.post(`${process.env.VITE_API_BASE_URL}/api/database-info/generate-chart-data/${database}/${schema}/${table}`, bodyRequest)
   const data = response.data
 
   return data
